273c540b6ea57e2e36ba7898ee772c346a4322c5
Pooyan Khosravi <pekhee@gmail.com>
2014-09-18 09:04:58 +0800

    rack-cors dep and conf for handling CORS

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'sqlite3'
 
 gem 'active_model_serializers', '~> 0.8.0'
 
+gem 'rack-cors', :require => 'rack/cors'
 
 
 # To use ActiveModel has_secure_password
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(*Rails.groups)
 
 module TheBack
   class Application < Rails::Application
+
+    # Rack-cors
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
